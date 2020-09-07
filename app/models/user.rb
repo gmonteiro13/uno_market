de06c49389b1004a408d77602eac5a3082c92fd5
gmonteiro13 <gscmonteiro@gmail.com>
2020-09-07 18:45:30 -0300

    user companysuccessfully created

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  before_create :set_company
+
+  private
+
+  def set_company
+    self.company = self.email.split("@").last.split(".").first.capitalize!
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'company' do
+    it 'generate on create' do
+      user = User.create!(email: 'biel@nintendo.com', password: '123456')
+
+      user.save
+      user.reload
+
+      expect(user.company).to be_present
+    end
+  end
 end
