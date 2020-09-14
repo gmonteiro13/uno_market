431e07ad2f7ef7e684e4679f2c50c3d776011b67
gmonteiro13 <gscmonteiro@gmail.com>
2020-09-13 22:51:31 -0300

    User does not view unavailable products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    @products = Product.where(status: :available) # Usuário vê apenas os produtos da empresa aqui em vez de usar if no index?
   end
 
   # GET /products/1
--- spec/features/user_start_deal_spec.rb
+++ spec/features/user_start_deal_spec.rb
@@ -10,11 +10,31 @@ feature 'User view products' do
         
         visit root_path
         click_on 'Ver todos os produtos'
-        click_on 'Ver'
+        click_on "Ver #{product.product_name}"
         click_on 'Comprar'
         click_on 'Iniciar compra'
 
         expect(product.deal).to be_open
         expect(product.reload).to be_unavailable
+    end
+
+    scenario 'does not view unavailable products' do
+        user = User.create!(email: 'biel@nintendo.com', password: '123456')
+        user_2 = User.create!(email: 'tiago@nintendo.com', password: '654321')
+        login_as user
+
+        product = Product.create!(product_name: 'Donkey Kong Country 2', category: 'Jogos', description: 'Jogo de aventura do SNES', price: 100, user: user_2, status: :available)
+        product_2 = Product.create!(product_name: 'Shinobi III', category: 'Jogos', description: 'Jogo com trilha sonora fantástica', price: 80, user: user_2, status: :available)
+
+        visit root_path
+        click_on 'Ver todos os produtos'
+        click_on "Ver #{product.product_name}"
+        click_on 'Comprar'
+        click_on 'Iniciar compra'
+        click_on 'Voltar'
+        
+        expect(product.reload).to be_unavailable
+        expect(page).not_to have_content('Donkey Kong Country 2')
+        expect(page).to have_content('Shinobi III')
     end
 end
