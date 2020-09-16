05a230358b3cff0364b28f7b7311335f9c97de45
gmonteiro13 <gscmonteiro@gmail.com>
2020-09-16 20:04:49 -0300

    User cannot create a product with blank attributes

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,5 +3,10 @@ class Product < ApplicationRecord
   has_many :comments, as: :commentable
   has_one :deal
 
+  validates :product_name, presence: true
+  validates :category, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+
   enum status: { available: 0, unavailable: 5, sold: 10 }
 end
--- spec/features/user_register_new_product_spec.rb
+++ spec/features/user_register_new_product_spec.rb
@@ -12,4 +12,23 @@ feature 'User register a valid product' do
         expect(page).to have_content('Você precisa ter um perfil para registrar um novo produto')
         expect(page).to have_link('Criar perfil')
     end
+
+    scenario 'and attributes cannot be blank' do
+        user = User.create!(email: 'biel@email.com', password: '123456')
+        profile = Profile.create!(full_name: 'Gabriel Soares Cardoso Monteiro', social_name: 'Gabriel Monteiro', 
+        birth_date: '14/12/1994', job: 'Level Designer', department: 'Jogos', user: user)
+
+        login_as user
+        visit root_path
+        click_on 'Ver todos os produtos'
+        click_on 'Novo produto'
+
+        fill_in 'Nome do produto', with: ''
+        select 'Jogos', from: 'Categoria'
+        fill_in 'Descrição', with: ''
+        fill_in 'Preço', with: ''
+        click_on 'Criar Produto'
+
+        expect(page).to have_content('não pode ficar em branco', count: 3)
+    end
 end
