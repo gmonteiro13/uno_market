75e277b9e5e4aad9e8ea65f11573d3db849592f0
gmonteiro13 <gscmonteiro@gmail.com>
2020-09-17 17:52:28 -0300

    User add reply to a comment

--- spec/features/user_comments_on_product_spec.rb
+++ spec/features/user_comments_on_product_spec.rb
@@ -18,4 +18,30 @@ feature 'User comments on a product' do
         expect(page).to have_content('Comentário de teste')
         expect(page).to have_content("Enviado a menos de um minuto atrás por #{user.profile.social_name}")
     end
+
+    scenario 'and add replies' do
+        user = User.create!(email: 'biel@nintendo.com', password: '123456')
+        user_2 = User.create!(email: 'tiago@nintendo.com', password: '654321')
+        profile = Profile.create!(full_name: 'Gabriel Soares Cardoso Monteiro', social_name: 'Gabriel Monteiro', 
+        birth_date: '14/12/1994', job: 'Level Designer', department: 'Jogos', user: user)
+        product = Product.create!(product_name: 'Donkey Kong Country 2', category: 'Jogos', description: 'Jogo de aventura do SNES', price: 100, user: user_2, status: :available)
+
+
+        login_as user
+        visit root_path
+        click_on 'Ver todos os produtos'
+        click_on "Ver #{product.product_name}"
+        fill_in 'comment_body', with: 'Comentário de teste'
+        click_on 'Comentar'
+
+        within('li') do
+        fill_in 'comment_body', with: 'Resposta de teste'
+        end
+        click_on 'Responder'
+
+
+        expect(page).to have_content('Comentário de teste')
+        expect(page).to have_content('Resposta de teste')
+        expect(page).to have_content("Enviado a menos de um minuto atrás por #{user.profile.social_name}")
+    end
 end
